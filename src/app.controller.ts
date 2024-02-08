5d03adcfba8c316b7aa31f45e0afb9a66b0c0df2
choewy <choewy32@gmail.com>
2024-02-08 17:19:34 +0900

    refactor/controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,38 +6,35 @@ import {
   UseGuards,
   UsePipes,
 } from '@nestjs/common';
-import { AppService } from './app.service';
 import { AuthGuard } from './guards/auth.guard';
 import { AlwaysFailedPipe } from './pipes/always-failed.pipe';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
   @Get('ok')
-  getOkResponse(): string {
-    return this.appService.getHello();
+  getOkResponse(): void {
+    return;
   }
 
   @Get('fail')
-  throwException(): string {
+  throwException(): never {
     throw new BadRequestException();
   }
 
   @Get('error')
-  throwError(): string {
+  throwError(): never {
     throw new Error('test error');
   }
 
   @Get('guard')
   @UseGuards(AuthGuard)
-  throwExceptionByGuard() {
+  throwExceptionByGuard(): void {
     return;
   }
 
   @Get('pipe')
   @UsePipes(AlwaysFailedPipe)
-  throwValidationPipe(@Param() params: never) {
+  throwValidationPipe(@Param() _: never): void {
     return;
   }
 }
